7626d553832c39f68de6cdc39708bb3114b1076f
Tim Visher <tim.visher@gmail.com>
2014-12-05 01:59:53 -0500

    Add the ability to write to a string.

--- src/clj/nhss/core.clj
+++ src/clj/nhss/core.clj
@@ -2,6 +2,14 @@
   (:require [cognitect.transit :as t]
             [clojure.java.io   :as io]))
 
-(defn write-transit-levels []
+(defn write-transit-levels-to-out [out]
+  (t/write (t/writer out :json) (read-string (slurp "levels.edn"))))
+
+(defn write-transit-levels-to-file []
   (with-open [out (io/output-stream "levels.json")]
-    (t/write (t/writer out :json) (read-string (slurp "levels.edn")))))
+    (write-transit-levels-to-out out)))
+
+(defn write-transit-levels-to-string []
+  (let [out (java.io.ByteArrayOutputStream. 4096)]
+    (write-transit-levels-to-out out)
+    (str out)))
