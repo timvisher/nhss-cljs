915a35b411b705c3c1717cec2af424d593ee4ef1
Tim Visher <tim.visher@gmail.com>
2014-12-08 16:17:54 -0500

    Can basically listen for events.

--- project.clj
+++ project.clj
@@ -4,9 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure        "1.6.0"]
+                 [org.clojure/clojurescript  "0.0-2411"]
                  [com.cognitect/transit-cljs "0.8.192"]
                  [org.clojure/core.async     "0.1.346.0-17112a-alpha"]]
   :source-paths ["src/clj" "src/cljs"]
   :profiles {:uberjar {:aot :all}
-             :dev     {:plugins      [[com.cemerick/austin       "0.1.5"]]
-                       :dependencies [[com.cognitect/transit-clj "0.8.259"]]}})
+             :dev     {:plugins      [[com.cemerick/austin       "0.1.5"]
+                                      [lein-cljsbuild            "1.0.3"]]
+                       :dependencies [[com.cognitect/transit-clj "0.8.259"]]}}
+  :cljsbuild {:builds [{:source-paths ["src/cljs"]
+                        :compiler {:optimizations :whitespace
+                                   :pretty-print  true}}]})
---
+++ src/cljs/nhss/ui.cljs
@@ -0,0 +1,40 @@
+(ns nhss.ui
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require [goog.events :as events]
+            [cljs.core.async :refer [put! chan <!!]])
+  (:import [goog.events KeyCodes]))
+
+(defn keys []
+  {KeyCodes/H              :w
+   KeyCodes/J              :s
+   KeyCodes/K              :n
+   KeyCodes/L              :e
+   KeyCodes/Y              :ny
+   KeyCodes/U              :ne
+   KeyCodes/B              :sw
+   KeyCodes/N              :se
+   KeyCodes/UP             :n
+   KeyCodes/RIGHT          :e
+   KeyCodes/DOWN           :s
+   KeyCodes/LEFT           :w
+   KeyCodes/NUM_NORTH_EAST :ne
+   KeyCodes/NUM_SOUTH_EAST :se
+   KeyCodes/NUM_SOUTH_WEST :sw
+   KeyCodes/NUM_NORTH_WEST :nw
+   KeyCodes/NUM_WEST       :w
+   KeyCodes/NUM_NORTH      :n
+   KeyCodes/NUM_EAST       :e
+   KeyCodes/NKUM_SOUTH     :s})
+
+(defn event->key [e]
+  (get (keys) (.-keyCode e) :key-not-found))
+
+(defn init []
+  (let [event-chan (chan)]
+    (events/listen (.-body js/document)
+                   (.-KEYUP events/EventType)
+                   (fn [e]
+                     (put! event-chan (event->key e))))
+    (go-loop [key (<! event-chan)]
+      (.log js/console key)
+      (recur (<! event-chan)))))
