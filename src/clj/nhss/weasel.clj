9badc5481f3bb54d8836b6bfd369a320744fb789
Tim Visher <tim.visher@gmail.com>
2014-12-19 15:32:34 -0500

    Adding weasel helpers.

---
+++ src/clj/nhss/weasel.clj
@@ -0,0 +1,7 @@
+(ns nhss.weasel
+  (:require cemerick.piggieback
+            weasel.repl.websocket))
+
+(cemerick.piggieback/cljs-repl
+ :repl-env (weasel.repl.websocket/repl-env
+            :ip "0.0.0.0" :port 9001))
